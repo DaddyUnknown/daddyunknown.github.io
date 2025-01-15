057517e89dcd9d9c354573da192324e48020f601
DaddyUnknown <150858017+DaddyUnknown@users.noreply.github.com>
2025-01-16 01:48:58 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://daddyunknown.github.io/'
     }
   },
   build: {
